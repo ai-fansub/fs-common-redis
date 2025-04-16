31d119c4fa07ca05a424ed4f1ecd234dd3fec3b8
SSHwan22 <hshwan0406@gmail.com>
2025-04-16 15:15:50 +0900

    [init] common-redis 생성
    common-redis 생성

---
+++ src/main/java/com/fs/common/redis/configuration/RedisConfigurationSelector.java
@@ -0,0 +1,15 @@
+package com.fs.common.redis.configuration;
+
+import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+public class RedisConfigurationSelector implements ImportSelector {
+    @Override
+    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+
+        return new String[]{
+                RedisConfig.class.getName()
+        };
+
+    }
+}
